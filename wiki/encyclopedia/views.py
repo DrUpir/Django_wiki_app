8be1b56d4ca072a961117fdedecc8e28d3f400dc
Gennady Agashkov <g.agashkov@gamil.com>
2020-10-08 18:01:19 +0200

    add textarea to create.html

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -12,7 +12,7 @@ logger = logging.getLogger(__name__)
 
 class CreateNew_Form (forms.Form):
     title = forms.CharField(label = "Title")
-    # MD = 
+    MD = forms.CharField(widget = forms.Textarea)
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
@@ -57,6 +57,18 @@ def search(request):
     })
 
 def create(request):
+    if request.method == "POST":
+        form = CreateNew_Form(request.POST)
+        if form.is_valid():
+            # check for existed title in entries
+            # check for correct MD format
+            #util.save_entry(r)
+            pass
+
+        return render(request, "encyclopedia/error.html", {
+            "error_string":  f"Error with POST create new entry"
+        })
+
     return render(request, "encyclopedia/create.html", {
         'form': CreateNew_Form()
     })
