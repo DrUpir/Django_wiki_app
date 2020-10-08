525ac40188d5277901a87a5b74c77bb87ba157b4
Gennady Agashkov <g.agashkov@gamil.com>
2020-10-08 11:11:12 +0200

    show list of matches entries when searching

--- Project1/wiki/encyclopedia/views.py
+++ Project1/wiki/encyclopedia/views.py
@@ -5,10 +5,15 @@ from django.http import HttpResponseRedirect, HttpResponseNotFound
 
 from . import util
 
+# import the logging library
+import logging
+
+# Get an instance of a logger
+logger = logging.getLogger(__name__)
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
-        "entries": util.list_entries()
+        "entries": util.get_list_entries()
     })
 
 def entry(request, title):
@@ -21,7 +26,24 @@ def entry(request, title):
     })
 
 def search(request):
-    if request.method == "GET" and request.GET.get("q"):
-        return HttpResponseRedirect(reverse("entry", kwargs={'title': request.GET.get("q")}))
+    if request.method == "GET":
+        search_string = request.GET.get("q")
+        logger.error(f"search_string = {search_string}")
+
+        if search_string:
+            list_all_entries = util.get_list_entries()
+            logger.error(f"list_all_entries = {list_all_entries}")
+
+            if search_string in list_all_entries:
+                return HttpResponseRedirect(reverse("entry", kwargs={'title': request.GET.get("q")}))
+
+            list_finded_entrys = [title for title in list_all_entries if search_string.lower() in title.lower()]
+            logger.error(f"list_finded_entrys = {list_finded_entrys}")
+
+            if list_finded_entrys:
+                return render(request, "encyclopedia/search.html", {
+                    "entries": list_finded_entrys    
+                })
+
     return HttpResponseNotFound('<h1>Error in search view</h1>')
    
