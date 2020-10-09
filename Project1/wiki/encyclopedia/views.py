622cdfc367a11ebf76e74e24d07a7bbad0ec046b
Gennady Agashkov <g.agashkov@gamil.com>
2020-10-09 14:46:27 +0200

    cleaned_data in create form

--- Project1/wiki/encyclopedia/views.py
+++ Project1/wiki/encyclopedia/views.py
@@ -35,17 +35,17 @@ def entry(request, title):
 def search(request):
     if request.method == "GET":
         search_string = request.GET.get("q")
-        logger.error(f"search_string = {search_string}")
+        # logger.error(f"search_string = {search_string}")
 
         if search_string:
             list_all_entries = util.get_list_entries()
-            logger.error(f"list_all_entries = {list_all_entries}")
+            # logger.error(f"list_all_entries = {list_all_entries}")
 
             if [s for s in list_all_entries if search_string.lower() == s.lower()]:
                 return HttpResponseRedirect(reverse("entry", kwargs={'title': search_string}))
 
             list_finded_entrys = [s for s in list_all_entries if search_string.lower() in s.lower()]
-            logger.error(f"list_finded_entrys = {list_finded_entrys}")
+            # logger.error(f"list_finded_entrys = {list_finded_entrys}")
 
             if list_finded_entrys:
                 return render(request, "encyclopedia/search.html", {
@@ -59,22 +59,34 @@ def search(request):
 def create(request):
     if request.method == "POST":
         form = CreateNew_Form(request.POST)
-        if form.is_valid():
-            
-            title = form.cleaned_data["title"]
-            # if entry allready exist return error
-            # Error will display on the same page to avoid data lost
-            if util.get_entry(title):
-                return render(request, "encyclopedia/create.html", {
+        
+        # if not valid form fields - return error
+
+        logger.error(f"form.cleaned_data {form.cleaned_data}")
+        logger.error(f"form.is_valid() = {form.is_valid()}")
+
+        if not form.is_valid():
+            return render(request, "encyclopedia/create.html", {
                     'form': form,
-                    'error_string': f"Error: Entry with title \"{title}\" already exist"
+                    'error_string': f"Error: not valid form fields"
                 })
-            # check for correct MD format
-            #util.save_entry(r)
-            pass
+            
+        title = form.cleaned_data["title"]
+
+        # if entry allready exist return error
+        # Error will display on the same page to avoid data lost
+        if util.get_entry(title):
+            return render(request, "encyclopedia/create.html", {
+                'form': form,
+                'error_string': f"Error: Entry with title \"{title}\" already exist"
+            })
+
+        #check for correct MD format
+        #util.save_entry(r)
+
 
         return render(request, "encyclopedia/error.html", {
-            "error_string":  f"Error: with processing POST request"
+            "error_string":  f"WHILE I DON'T SEE LOGGING ERROR IN CONSOLE Error: with processing POST request"
         })
 
     return render(request, "encyclopedia/create.html", {
