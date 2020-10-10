0459ebdd8da0bfcceb66d93e234a5501216b0b4e
GennadyAgashkov <g.agashkov@gmail.com>
2020-10-10 21:19:20 +0200

    add edit() view, edit.html and link to edit on entry.html

--- Project1/wiki/wiki/settings.py
+++ Project1/wiki/wiki/settings.py
@@ -12,20 +12,6 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'handlers': {
-        'console': {
-            'class': 'logging.StreamHandler',
-        },
-    },
-    'root': {
-        'handlers': ['console'],
-        'level': 'WARNING',
-    },
-}
-
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
