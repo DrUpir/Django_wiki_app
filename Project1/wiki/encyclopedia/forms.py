401172aa7423738c980b2fa2c8bf5f2a982ebca0
GennadyAgashkov <g.agashkov@gmail.com>
2020-10-15 07:19:22 +0200

    add some cosmetic changese in forms.py
    change class name & change design of class: indenting parametrs

--- Project1/wiki/encyclopedia/forms.py
+++ Project1/wiki/encyclopedia/forms.py
@@ -1,11 +1,15 @@
 from django import forms
 
-class CreateNew_Form (forms.Form):
-    title = forms.CharField(label = "Title", 
-        widget = forms.TextInput(attrs={
-            'class': 'form-control'
+class EditEntryForm (forms.Form):
+    title = forms.CharField(
+        label = "Title", 
+        widget = forms.TextInput(
+            attrs={
+                'class': 'form-control'
         }))
-    md_content = forms.CharField(label = "MarkDown content",
-        widget = forms.Textarea(attrs={
-            'class': 'form-control'
+    md_content = forms.CharField(
+        label = "MarkDown content",
+        widget = forms.Textarea(
+            attrs={
+                'class': 'form-control'
         }))
